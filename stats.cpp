6b99311ac73662cb621a9be7b72c1c8a24bf6804
Yehya Juma <yehya3213@hotmail.com>
2023-10-27 10:52:58 +0100

    initial commit

---
+++ stats.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  int no1;
+  int no2;
+  cout << "please enter the first number: ";
+  cin>> no1;
+  cout <<"please enter the second number: ";
+  cin>> no2;
+
+  cout<<"the sum of "<<no1<<" and " << no2 <<" is = "<< no1 + no2 <<"\n";
+  cout<<"the difference of "<<no1<<" and " <<no2<<" is = "<<no1 - no2<<"\n";
+  cout<<"the product of "<<no1<<" and " <<no2<<" is = "<<no1 *  no2<<"\n";
+  cout<<"the average of "<<no1<<" and " <<no2<<" is = "<<( no1 +  no2)/2 <<"\n";
+
+}
